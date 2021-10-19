d34db0cfd4209897424e2c42d97e75a6c62ba2dd
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-19 09:27:24 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.py
@@ -0,0 +1,5 @@
+for index in range(2,20):
+    if index == 8:
+        basic.show_icon(IconNames.HEART)
+    else:
+        basic.show_number(index)
---
+++ main.ts
@@ -0,0 +1,8 @@
+for (let index = 2; index < 20; index++) {
+    if (index == 8) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showNumber(index)
+    }
+    
+}
